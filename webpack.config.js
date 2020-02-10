cd3f15fe270662201ac4c04a2b5a26cfac8ed52a
Xuffasch <tristan.lucas@gmail.com>
2020-02-10 10:53:25 +0100

    Basic webpack dev server setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Webpack tutorial",
     })
-  ]
+  ],
+  devServer: {
+    // Display only errors to reduce output
+    stats: "errors-only",
+    // Parse host and port from .env 
+    // When using Docker, Vagrant or Cloud9 set host: "0.0.0.0"
+
+    // 0.0.0.0 is available to all network devices
+    // unlike default `localhost`.
+    host: process.env.HOST, // Defaults to `localhost`
+    port: process.env.PORT, // Defaults to 8080
+    open: true, // open the page in browser
+  },
 }
